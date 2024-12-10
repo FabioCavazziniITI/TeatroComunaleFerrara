afdc7dde229ccc21db36e22a4d537945cdc169f8
Fabio Cavazzini <fabio.cavazzini@iticopernico.it>
2024-12-10 23:56:28 +0100

    edit form

---
+++ php/biglietti.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="https://www.teatrocomunaleferrara.it/wp-content/uploads/Teatro-comunale-di-Ferrara-logo-bianco.svg">
+        <link rel="stylesheet" type="text/css" href="../css/cssPHP.css" media="all">
+        <title>
+            Teatro Comunale Ferrara
+        </title>
+    </head>
+    <body>
+        <!--CONTENUTO-->
+        
+    </body>
+</html>
