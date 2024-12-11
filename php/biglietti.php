430babb4bdc37d4fde410520afc3de176827ff2b
Fabio Cavazzini <fabio.cavazzini@iticopernico.it>
2024-12-11 10:27:08 +0100

    edit page PHP

--- php/biglietti.php
+++ php/biglietti.php
@@ -1,13 +1,91 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="icon" href="https://cdn.icon-icons.com/icons2/2869/PNG/512/order_confirmation_icon_181878.png">
+        <link rel="icon" href="https://www.teatrocomunaleferrara.it/wp-content/uploads/Teatro-comunale-di-Ferrara-logo-bianco.svg">
         <link rel="stylesheet" type="text/css" href="../css/cssPHP.css" media="all">
         <title>
-            Conferma Ordine
+            Teatro Comunale Ferrara
         </title>
     </head>
     <body>
-    	Ciao <?php echo ($_POST["nome "+"cognome"]);?>.<br>
+        <div class="header">
+            <!--LOGO-->
+            <div class="logo">
+                <a href="../index.html">
+                    <img src="https://www.teatrocomunaleferrara.it/wp-content/uploads/Teatro-comunale-di-Ferrara-logo-bianco.svg" alt="logo" class="img_logo">
+                </a>
+                <p class="name_sito">
+                    <a href="../index.html">
+                        Teatro Comunale Ferrara
+                    </a>
+                </p>
+            </div>
+    
+            <!--MENU-->
+            <div class="nav">
+                <ul>
+                    <li>
+                        <a href="../index.html">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../html/prenota.html">
+                            <u class="now">
+                                Prenota
+                            </u>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="containerPHP">
+            Ciao
+            <?php
+                //saluto
+                echo ($_POST["nome"]). "&nbsp;";
+                echo ($_POST["cognome"]). ",<br>";
+                
+                //stampa valore assunto da "zona"
+                //var_dump($_POST["zona"]);
+                
+                //cosa fare in base alla scelta zona
+                if (($_POST["zona"]) == "divisorio") {
+                    echo ("Hai selezionato un divisore di zona.
+                            &Egrave; necessario scegliere una delle zone <u>presenti</u> nel <b>listino prezzi</b>!");
+                }
+                
+                else {
+                    echo ("opzione non ancora inserita nel file PHP");
+                }
+            ?>
+        </div>
+
+        <!--FOOTER-->
+        <div class="footer">
+            <div class="footer_div">
+              <a href="http://www.iiscopernico.edu.it/" target="_blank">
+                <img class="img_footer" src="../img/Stemma_ITI.png" alt="Logo ITI cliccabile">
+              </a>
+              <br>
+              <I>
+                <a href="http://www.iiscopernico.edu.it/" target="_blank">
+                    IIS "N.Copernico - Carpeggiani"
+                </a>
+              </I>
+            </div>
+            <div class="footer_div">
+              &#0169 Cavazzini Fabio
+              </br></br>
+              <a href="mailto:fabio.cavazzini@iticopernico.it">
+                  fabio.cavazzini@iticopernico.it
+              </a>
+              </br></br>
+                Classe V G
+              </br></br>
+                A.S. 2024 - 2025
+            </div>
+          </div>
     </body>
 </html>
